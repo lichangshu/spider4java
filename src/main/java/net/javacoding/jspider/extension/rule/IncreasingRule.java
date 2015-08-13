47fcd760c2b1ea2bf459ba49a6f9bef067f16679
changshu.li <lcs.005@163.com>
2015-08-13 15:18:37 +0800

    increasing  in batches

--- src/main/java/net/javacoding/jspider/extension/rule/IncreasingRule.java
+++ src/main/java/net/javacoding/jspider/extension/rule/IncreasingRule.java
@@ -9,7 +9,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.MessageFormat;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -31,7 +33,6 @@ import net.javacoding.jspider.core.util.config.PropertySet;
 public class IncreasingRule extends BaseRuleImpl {
 
 	private static final Log log = LogFactory.getLog(AcceptPattenUrlOnlyRule.class);
-	private static final Set<String> set = new ConcurrentSkipListSet();
 
 	public static final String FROM = "from";
 	public static final String TO = "to";
@@ -60,9 +61,67 @@ public class IncreasingRule extends BaseRuleImpl {
 		log.info("load pattern " + from + " to " + to);
 	}
 
+//	private static final Set<String> set = new ConcurrentSkipListSet();
+//	@Override
+//	public Decision apply(SpiderContext context, Site currentSite, URL url) {
+//		if (!set.contains(url.toString())) {
+//			String path = url.getPath();
+//			if (this.queryEnable) {
+//				if (url.getQuery() != null) {
+//					path += "?" + url.getQuery();
+//				}
+//			}
+//			Matcher mt = pattern.matcher(path);
+//			if (mt.matches()) {
+//				int size = mt.groupCount() + 1;
+//				Object[] pts = new Object[size + 1];
+//				for (int i = 0; i < size; i++) {
+//					pts[i] = mt.group(i);
+//				}
+//				for (int i = from; i <= to; i++) {
+//					if (i % 20 == 0) {
+//						log.info(String.format("Increasing page for [%s], from [%d] to [%d]!", url, from, i));
+//					}
+//					pts[size] = i;
+//					String fm = MessageFormat.format(replace, pts);
+//					try {
+//						URL find = URLUtil.normalize(new URL(url, fm));
+//						set.add(find.toString());
+//						context.getAgent().registerEvent(url, new URLFoundEvent(context, url, find));
+//						log.debug("Increasing page " + find);
+//					} catch (MalformedURLException ex) {
+//						log.error(ex);
+//					}
+//				}
+//				log.info(String.format("Increasing page for [%s], from [%d] to [%d]!", url, from, to));
+//			}
+//		}
+//		return new DecisionInternal(DecisionInternal.RULE_ACCEPT);
+//	}
+	/**
+	 * Object[] 数组第一个元素表示整个匹配的字符,<br/>
+	 * 最后一个元素表示要顺序增加的数字. <br/>
+	 * 其他部分则是正则表达式的匹配组!!
+	 */
+	private final Map<String, Object[]> map = new ConcurrentHashMap();
+
+	/**
+	 *
+	 *
+	 * @param context
+	 * @param currentSite
+	 * @param url
+	 * @return
+	 */
 	@Override
 	public Decision apply(SpiderContext context, Site currentSite, URL url) {
-		if (!set.contains(url.toString())) {
+		URL orgin = url;
+		Object[] pts = null;
+		if (map.containsKey(url.toString())) {
+			// remove !
+			pts = map.remove(url.toString());
+			orgin = (URL) pts[0];
+		} else {
 			String path = url.getPath();
 			if (this.queryEnable) {
 				if (url.getQuery() != null) {
@@ -71,30 +130,39 @@ public class IncreasingRule extends BaseRuleImpl {
 			}
 			Matcher mt = pattern.matcher(path);
 			if (mt.matches()) {
-				int size = mt.groupCount() + 1;
-				Object[] pts = new Object[size + 1];
-				for (int i = 0; i < size; i++) {
+				orgin = url;
+				int size = mt.groupCount();
+				pts = new Object[size + 2];
+				pts[0] = url;
+				for (int i = 1; i <= size; i++) {
 					pts[i] = mt.group(i);
 				}
-				for (int i = from; i <= to; i++) {
-					if (i % 20 == 0) {
-						log.info(String.format("Increasing page for [%s], from [%d] to [%d]!", url, from, i));
-					}
-					pts[size] = i;
-					String fm = MessageFormat.format(replace, pts);
-					try {
-						URL find = URLUtil.normalize(new URL(url, fm));
-						set.add(find.toString());
-						context.getAgent().registerEvent(url, new URLFoundEvent(context, url, find));
-						log.debug("Increasing page " + find);
-					} catch (MalformedURLException ex) {
-						log.error(ex);
-					}
-				}
-				log.info(String.format("Increasing page for [%s], from [%d] to [%d]!", url, from, to));
+			}
+		}
+		if (pts != null) {
+			URL find = this.getNexturl(pts, orgin);
+			if (find != null) {
+				log.debug("Increasing page " + find);
+				map.put(find.toString(), pts);
+				context.getAgent().registerEvent(orgin, new URLFoundEvent(context, orgin, find));
 			}
 		}
 		return new DecisionInternal(DecisionInternal.RULE_ACCEPT);
 	}
 
+	public URL getNexturl(Object[] pts, URL url) {
+		int size = pts.length - 1;
+		// hear + 1
+		pts[size] = pts[size] == null ? this.from : ((Integer) pts[size]) + 1;
+		if (((Integer) pts[size]) <= this.to) {
+			String fm = MessageFormat.format(replace, pts);
+			try {
+				URL find = URLUtil.normalize(new URL(url, fm));
+				return find;
+			} catch (MalformedURLException ex) {
+				log.error(ex);
+			}
+		}
+		return null;
+	}
 }
